67a88cebe06ff66bdf938a5d3f9c205ee7f0fa1b
Jorge <jmigueles20@alumnos.utalca.cl>
2026-02-16 16:23:44 -0300

    minor fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import installationRoutes from './installation.routes';
 import technicianRoutes from './technician.routes';
+import odbRoutes from './odb';
 
 const router = Router();
 
 router.use('/installations', installationRoutes);
 router.use('/technicians', technicianRoutes);
+router.use('/odb', odbRoutes); 
+
 
 export default router;
